3a78c15b9db2fb68f9e237ffc1b8973c35090b14
Alexander Nikolaev <Nikolaevsailing@mail.ru>
2021-05-28 17:10:27 +0400

    fix: section

--- src/scripts/section.js
+++ src/scripts/section.js
@@ -206,18 +206,16 @@ export class Section extends React.Component {
             <div class="article__title">{this.findText("portfolio").title}</div>
           </div>
           <div class="article__wrap">
-            <Link link="https://sitecreator143.github.io/cv-react/dist" title="CV (React, HTML/SCSS, Webpack)"/>
-            <Link link="https://sitecreator143.github.io/kids-jump-tech/dist" title="Landing: Kids jump tech (HTML/SCSS/JS, Webpack)"/>
+            <Link link="https://sitecreator143.github.io/cv-react/dist" title="CV (React, Pug/SCSS, Webpack)"/>
+            <Link link="https://sitecreator143.github.io/site-landing-kids-jump-tech/dist" title="Landing: Kids jump tech (HTML/SCSS/JS, Webpack)"/>
             <Link link="https://sitecreator143.github.io/RS-clone" title="App: Dice poker (HTML/CSS/JS, AnimeJS, AudioAPI, Mocha, Airbnb)"/>
             <Link link="https://sitecreator143.github.io/app-english-for-kids" title="App: English for kids (HTML/SCSS/JS)"/>
             <Link link="https://sitecreator143.github.io/app-covid-dashboard/" title="App: Covid dashboard (HTML/SCSS/JS, Hight charts, ESlint, API, localStorage)"/>
             <Link link="https://sitecreator143.github.io/app-gem-puzzle" title="App: Gem puzzle (HTML/SCSS/JS, localStorage, Canvas)"/>
             <Link link="https://sitecreator143.github.io/app-keyboard" title="App: Keyboard (HTML/SCSS/JS)"/>
-            <Link link="https://sitecreator143.github.io/app-calculator" title="App: Calculator (HTML/CSS/JS)"/>
+            <Link link="https://sitecreator143.github.io/app-calculator/dist" title="App: Calculator (HTML/CSS/JS)"/>
             <Link link="https://sitecreator143.github.io/site-landing-shelter" title="Landing: Shelter (HTML/SCSS/JS)"/>
             <Link link="https://sitecreator143.github.io/site-landing-eleven" title="Landing: Eleven (HTML/SCSS/JS, jQuery, PHP mailer)"/>
-            <Link link="https://sitecreator143.github.io/app-momentum/" title="App: Momentum (HTML/SCSS/JS, API, localStorage)"/>
-            <Link link="https://sitecreator143.github.io/site-landing-portfolio" title="Landing: Personal page (HTML/SCSS/jQuery)"/>
             <Link link="https://sitecreator143.github.io/site-landing-webdev" title="Landing: Webdev (HTML/CSS)"/>
             <Link link="https://github.com/Sitecreator143" title="My GitHub: Sitecreator143"/>
           </div>
